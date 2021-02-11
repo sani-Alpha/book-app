f514d58b45367db316df94a4d2760b362e2b6a80
Sanidhya Rai <rai.sanidhya2010@gmail.com>
2021-02-11 21:25:53 +0530

    Added Mongo Support

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoclient = require('mongodb').MongoClient; 
+// const objectid = require('mongodb').ObjectID;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+let database,collection;
+
+app.post('/books',(req,res) => {
+    console.log(req.body);
+    collection.insert(req.body, (error,result) => {
+        if(error)
+            return res.status(500).send(error);
+        res.send(result.result);
+    });
+});
+
+app.get('/books', function(req,res) {
+    collection.find({}).toArray((error,result) =>{
+        if(error)
+            return res.status(500).send(error); 
+        res.send(result);
+    });
+});
+
+app.listen(3000, ()=>{
+    mongoclient.connect('mongodb://localhost/my_books', {useNewUrlParser: true}, (error,client) => {
+        if(error)
+            throw error;
+        database = client.db('my_books');
+        collection = database.collection('library');
+        console.log('connected to my_books');
+    });    
+    console.log('Listening on http://localhost:3000/books');
+});
